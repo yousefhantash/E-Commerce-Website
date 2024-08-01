cf30ab69bff5850d03364006d9e6bc4b77536749
yousefhantash <yousef.pizza17@gmail.com>
2024-08-01 13:35:09 +0300

    yousef

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div>
+
+
+    <div className='bg-amber-100 h-full'>
+
      <Footer/>
+     <h1>yousef</h1>
     </div>
   );
 }
