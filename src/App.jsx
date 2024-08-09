cab5386366e3859bfa6a8d730f251cd66ad9bbd0
yousefhantash <yousef.pizza17@gmail.com>
2024-08-09 17:24:52 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
 
 
-    <div className='bg-amber-100 h-full'>
-
-     <Footer/>
-     <h1>yousef</h1>
+    <div className='bg-amber-100 h-full flex justify-center '>
+    
     </div>
   );
 }
